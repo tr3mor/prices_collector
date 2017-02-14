0a3be11f4e191f5448af8a2a1943f3ebafbb9047
tr3mor <storozhenkopf@gmail.com>
2017-02-14 18:47:00 +0300

    added 1c and rechecked

--- ulmart_pars_test.py
+++ ulmart_pars_test.py
@@ -6,6 +6,7 @@ from bs4 import BeautifulSoup
 import datetime
 import smtplib
 from email.mime.text import MIMEText
+import re
 
 
 # func to send email via gmail
@@ -62,7 +63,8 @@ def get_last_raw(table_name):
 def inserting_data(next_seq, date, data, table_name):
     data.insert(0, next_seq)
     data.insert(0, date)
-
+    while len(data) != 5:
+        data.append('0')
     # inserting data from list to sql via
     query = "INSERT into "+table_name+" VALUES (%s,%s,%s,%s,%s)"
 
@@ -71,6 +73,7 @@ def inserting_data(next_seq, date, data, table_name):
     return data
 
 def ulmart_parser(urls):
+    data = []
     for url in urls:
         page = urllib.urlopen(url)
         soup = BeautifulSoup(page, "html.parser")
@@ -80,12 +83,28 @@ def ulmart_parser(urls):
         data.append(format(float(price[position:position2]), '.2f'))
     return data
 
+
+def interes_parser(urls):
+    data = []
+    for url in urls:
+        page = urllib.urlopen(url)
+        soup = BeautifulSoup(page, "html.parser")
+        price = str(soup.find_all('ins', class_="fl_l"))
+        data.append(format(float(re.findall('(\d+)', price)[0]), '.2f'))
+    return data
+
 # ========================================================================================================================================
 # dict for translating items to names
 dict_of_ulmart = {
     2: '<a href="https://www.ulmart.ru/goods/3949632">Видеокарта Gigabyte GeForce® GTX 1060</a>',
     3: '<a href="https://www.ulmart.ru/goods/3953858">Видеокарта ASUS GeForce® GTX 1060</a>',
     4: '<a href="https://www.ulmart.ru/goods/3886464">Видеокарта Gigabyte GeForce® GTX 1080</a>'}
+dict_of_interes = {2:'<a href="https://www.1c-interes.ru/catalog/all6968/23177156/">DualShock 4 Cont Magma Red(красный)</a>'}
+
+ulmart_urls = ["https://www.ulmart.ru/goods/3949632", 'https://www.ulmart.ru/goods/3953858',
+        "https://www.ulmart.ru/goods/3886464"]
+interes_urls = ["https://www.1c-interes.ru/catalog/all6968/23177156/"]
+
 
 # preparing today's date to insert
 date = datetime.datetime.now()
@@ -101,18 +120,30 @@ cursor = cnx.cursor()
 #parsing
 data = []
 next_seq = 1
-ulmart_urls = ["https://www.ulmart.ru/goods/3949632", 'https://www.ulmart.ru/goods/3953858',
-        "https://www.ulmart.ru/goods/3886464"]
-ulmart_data = ulmart_parser(ulmart_urls)
-ulmart_old_data, ulmart_last_seq = get_last_raw('Ulmart')
-ulmart_data = inserting_data(ulmart_last_seq, date, ulmart_data, 'Ulmart')
-price_changed, mail_text = Compare_prices(ulmart_old_data, ulmart_data, dict_of_ulmart)
 
+
+# Ulmart part
+# ---------------------------------------------------------------
+ulmart_data = ulmart_parser(ulmart_urls)
+ulmart_old_data, ulmart_next_seq = get_last_raw('Ulmart')
+ulmart_data = inserting_data(ulmart_next_seq, date, ulmart_data, 'Ulmart')
+price_changed_ulmart, mail_text_ulmart = Compare_prices(ulmart_old_data, ulmart_data, dict_of_ulmart)
+# --------------------------------------------------------------
+
+# 1c_interes part
+# -------------------------------------------------------------------
+interes_data = interes_parser(interes_urls)
+for x in interes_data:
+    print(x)
+interes_old_data, interes_next_seq = get_last_raw('Interes')
+print (interes_next_seq)
+interes_data = inserting_data(interes_next_seq,date,interes_data,'Interes')
+price_changed_interes, mail_text_interes= Compare_prices(interes_old_data,interes_data,dict_of_interes)
 
 cnx.commit()
 cnx.close()
 
-if price_changed:
-    send_email(mail_text)
+if (price_changed_interes or price_changed_ulmart):
+    send_email(mail_text_ulmart+mail_text_interes)
 
 
